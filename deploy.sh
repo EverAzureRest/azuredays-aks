a5d4181277c49c92c979817ebfdac1ac4826c44d
Jordan Smith <jorsmith@microsoft.com>
2019-02-04 09:30:42 -0800

    full release

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,11 @@ export AKS_SERVICE_GROUP=aksminecraft-server
 export AKS_SERVICE_NAME=aksminecraft-aks
 export ACR_NAME=myacrname
 export LOCATION=westus2
+export STORAGE_ACCOUNT_NAME=mystorageaccount
 #export VNET_NAME=
 #export VNET_RESOURCE_GROUP=
 #export SUBNET_NAME=
-#export SERVICE_PRINCIPAL_ID
+#export SERVICE_PRINCIPAL_ID=
 
 
 #Run az login if not already logged into a valid Azure subscription
@@ -17,6 +18,9 @@ SESSION_SUB=$(az account show --query "name" -o tsv)
 
 if [ $SESSION_SUB != $SUBSCRIPTION_NAME ]; then
     az account set -s $SUBSCRIPTION_NAME
+elif [ ! $SESSION_SUB ]; then
+    echo "Please login using 'az login'"
+    exit 1
 fi
 
 #create resource group for AKS service
@@ -59,16 +63,28 @@ az acr login -n $ACR_NAME
 #pull itzg/minecraft-server image from docker hub
 docker pull itzg/minecraft-server:latest
 
-#then we are going to blatently push his image to our own private repo \
+#then we are going to push his image to our own private repo \
 #but here we could also customize this image and create a docker file to build ourselves \
-#or have ACR build it.  See 'az acr build --help' for more info
+#or have ACR build it.  See 'az acr build --help' for more info.
 
 docker tag itzg/minecraft-server:latest $ACR_NAME.azurecr.io/minecraft-server:latest
 docker push $ACR_NAME.azurecr.io/minecraft-server:latest
 
-#Building a storage account in the managed Resource Group gives our ServicePrincipal the correct \
+#Build a storage account in the managed Resource Group. This gives our ServicePrincipal the correct \
 #permissions to manage dynamic volume claims. 
 
 MANAGED_GROUP=MC_${AKS_SERVICE_GROUP}_${AKS_SERVICE_NAME}_${LOCATION}
 
-az storage account create -n ##TODO
+if [ $(az storage account check-name -n $STORAGE_ACCOUNT_NAME --query "nameavailable") = false ]; then
+    RANDO=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 4 ; echo '')
+    STORAGE_ACCOUNT_NAME=${STORAGE_ACCOUNT_NAME}${RANDO}
+fi
+
+az storage account create -n $STORAGE_ACCOUNT_NAME -g $MANAGED_GROUP
+
+echo "Deployment of Azure Resources complete.  Please edit minecraft-final.yaml with the ACR Name ${ACRNAME} and \
+storage-class.yaml with the Storage Account Name ${STORAGE_ACCOUNT_NAME}"
+
+#Get Public IP Address after applying Kubernetes Config \
+# az network public-ip list -g $MANAGED_GROUP --query "[].ipAddress" -o tsv \
+# Connect your Minecraft client to this address. 
